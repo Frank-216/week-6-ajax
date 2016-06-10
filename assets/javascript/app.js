65ce496e1c1be1062a8fb4f9206e19c4511ac4d6
Frank Capraro <Frank@Franks-MacBook-Pro.local>
2016-06-10 18:39:38 -0400

    Almost done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,36 @@
 $(document).ready(function(){
+      
+    $('#submitBox').on('click', function() {
+        // Get a value from the input box
+            var newItem = $("#addSearch").val();
+            console.log(newItem);
+      
+        var button = $('<button>');
+        button.addClass('btn btn-default gif');
+        button.attr('data-item', newItem);
+        console.log(button.data('item'));
+        button.text(newItem);
+        console.log(button)
+            //add button to div holder
+        $('.button-holder').append(button);
+            // clear the submit box 
+        $('#addSearch').val('');
+        
+        //create new button
+        
 
-    $('button').on('click', function() {
+        return false;
+        
+    });
+// create two images.  One holds the still image with a still class, the other holds a gif image with a gif class
+// originally display the still image and hide the gif image.  
+//When the over event takes place hide the stiff image and display the gif image and vice versa
+    $('img').on('click',function(){
+        console.log('Mouse is over image');
+        $(this).addClass('check') ;   
+    });
+
+    $(document.body).on('click', '.gif', function(){
         $('#gifsAppearHere').empty();
         var animal = $(this).data('item');
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC";
@@ -29,40 +59,41 @@ $(document).ready(function(){
                     console.log(rating);
                     p.text('Rating: '+rating );
 
-                    var img = $('<img>');
-                    img.attr('src',results[i].images.fixed_height.url);
+                    var imgGif = $('<img>');
+                    var imgStill = $('<img>');
+                    // add a class that will stop the gif when it is loaded. Make the img src a still of the image. On hover switch it to the moving gif. 
+                    imgStill.attr('src',results[i].images.fixed_height_still.url);
+                    imgStill.addClass("still");
 
+                    //create img Gif
+                    imgGif.attr('scr', results[i].images.fixed_height.url);
+                    imgGif.addClass('move');
+
+                    // hide gif image
+                    imgGif.hide();
+                    // Add all items to the div
                     animalDiv.append(p);
-                    animalDiv.append(img);
+                    animalDiv.append(imgStill);
+                    animalDiv.append(imgGif);
                     $('#gifsAppearHere').prepend(animalDiv);
                     
                 };
+                $('img').hover(function(){
+                    console.log("entered the image")
+                    imgStill.hide();
+                    imgGif.show();
+                }, function(){
+                    imgStill.show();
+                    imgGif.hide();
+                });
+
+                
 
             });
-    });
-    $(document.body).on('click', '#submitBox', function(){
+
         
-    // Get a value from the input box
-        var newItem = $("#addSearch").val();
-        console.log(newItem);
+    });
 
-    //create new button
-    var button = $('<button>');
-    button.attr('data-item', newItem);
-    button.addClass('btn btn-default');
-    button.text(newItem);
-    console.log(button)
-    debugger;
-    //add button to div holder
-    $('.button-holder').append(button);
-
-
-
-
-    // Empty the specific <p> element that previously held the todo item.
-
-
-});
 
 });
 
